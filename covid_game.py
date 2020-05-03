14c80552559d7479bd57d8d993205feb212d52bf
elham_kh <elham.kheradmand@polymtl.ca>
2020-05-02 21:47:42 -0400

    made covid game compatible with branch

--- covid_game.py
+++ covid_game.py
@@ -1,8 +1,11 @@
 from modules import CovidRaid
+import time
 
-game = CovidRaid(preset='Covid', block_size=5, init_enemy_spawn=300, init_people_spawn=500, 
+env = CovidRaid(preset='AI', ai_agent=True, init_enemy_spawn=300, init_people_spawn=500, 
                 init_prop_spawn=50, init_fuel_spawn=800)
 
 is_running = True 
 while is_running:
-    is_running = game.update()
+    is_running = env.step(env.action_space.sample())
+    env.render()
+    # time.sleep(0.1)
